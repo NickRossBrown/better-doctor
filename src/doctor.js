c9e1e37fde519a22080b8481fb53d288fab11c55
Nick Brown <nickrossbrown@gmail.com>
2018-08-10 18:46:23 -0700

    fetch and display a phone number for each doctor. Results are now specified in the portland area

--- src/doctor.js
+++ src/doctor.js
@@ -6,9 +6,11 @@ export class Doctor {
 
   getDoctor(name) {
     return new Promise((resolve, reject) => {
+      let location = "45.5122%2C-122.6587%2C100"
 
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&user_key=${process.env.apiKey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&name=${name}&user_key=${process.env.apiKey}`
+
       request.onload = function() {
         if(request.status == 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -17,35 +17,38 @@ $(document).ready(function() {
     console.log(doc)
 
     doc.then( (result) => {
-      console.log(result);
       let docs = JSON.parse(result);
-      console.log(docs.meta)
-      console.log(docs.data)
-      console.log(docs.data[0].profile)
-      console.log(docs.data[0].profile.first_name)
-      console.log(docs.data[0].profile.last_name)
+      if (docs.meta.total===0) {
+        $(".results").append(`<p>No Search Results</p>`)
+      } else if (docs.meta.total > 0) {
+        $(".results").append(`<p>${docs.meta.total} Results</p>`)
+      }
+      // console.log(docs.data[0].profile)
+      // console.log(docs.data[0].profile.first_name)
+      // console.log(docs.data[0].profile.last_name)
     })
     doc.then((results) => {
       let doctors = JSON.parse(results);
+      console.log(doctors)
       for (let x in doctors.data) {
-        console.log(x)
-        console.log(doctors.data[x].profile.first_name)
-        console.log(doctors.data[x].profile.last_name)
+
+        console.log(doctors.data[x].practices[0].phones[0].number)
+
+
         $("#doctorTable").append(`<tr>
           <td>${doctors.data[x].profile.first_name}</td>
           <td>${doctors.data[x].profile.last_name}</td>
+          <td>${doctors.data[x].profile.last_name}</td>
+          <td>${doctors.data[x].practices[0].phones[0].number}</td>
         </tr>`)
       }
     })
-    // let newDocs = JSON.parse(doc)
-    //
-    // console.log(newDocs)
+    let newDocs = JSON.parse(doc)
+
+    console.log(newDocs)
   })
 
-  // $("#userInput").submit(function(event) {
-  //   console.log("hello")
-  //   event.preventDefault();
-  // });
+
 
 
 });
