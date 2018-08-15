c1b50f9be6690d4c38d923d81d719a7446d9a21d
Nick Brown <nickrossbrown@gmail.com>
2018-08-14 20:10:19 -0700

    find a doctor by the doctor's unique id and display every listed practice

--- src/doctor.js
+++ src/doctor.js
@@ -4,12 +4,12 @@ export class Doctor {
 
   }
 
-  getDoctor(name) {
+  getDoctors(search) {
     return new Promise((resolve, reject) => {
       let location = "45.5122%2C-122.6587%2C100"
       console.log(process.env.apiKey)
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&name=${name}&user_key=${process.env.apiKey}`
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&name=${search}&user_key=${process.env.apiKey}`
 
       request.onload = function() {
         if(request.status == 200) {
@@ -21,6 +21,26 @@ export class Doctor {
       request.open("GET", url);
       request.send();
 
+    });
+  }
+
+  findDoctor(doctorID) {
+    return new Promise((resolve, reject) => {
+      console.log(doctorID)
+      let request = new XMLHttpRequest();
+      let api_key = process.env.apiKey
+      let doctor_uid = doctorID
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors/${doctor_uid}?user_key=${api_key}`;
+
+      request.onload = function() {
+        if(request.status == 200) {
+          resolve(request.response);
+        } else {
+          reject(request.statusText);
+        }
+      }
+      request.open("GET", url);
+      request.send();
     });
   }
 
--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     let doctor = $("#doctorName").val();
     console.log(doctor)
     let getDoc = new Doctor ();
-    let doc = getDoc.getDoctor(doctor);
+    let doc = getDoc.getDoctors(doctor);
     console.log(doc)
 
     doc.then( (result) => {
@@ -30,19 +30,59 @@ $(document).ready(function() {
       console.log(doctors)
       for (let x in doctors.data) {
         $("#doctorTable").append(`<tr>
-          <td class="clickable" id="doctors.data[x].uid">${doctors.data[x].profile.first_name} ${doctors.data[x].profile.last_name}</td>
+          <td class="clickable" id="${doctors.data[x].uid}">${doctors.data[x].profile.first_name} ${doctors.data[x].profile.last_name}</td>
           <td>${doctors.data[x].profile.last_name}</td>
           <td>${doctors.data[x].practices[0].phones[0].number}</td>
         </tr>`)
       }
+
+      $(".clickable").each(function() {
+        $(this).click(function() {
+          console.log("clicked")
+          findDoc(this.id);
+
+
+        });
+      });
+
+
     })
 
-    $(".clickable").each(function() {
-      $(this).click(function() {
-        console.log("clicked")
+    function findDoc(docID) {
+      let getDoctor = new Doctor () ;
+      let promise = getDoctor.findDoctor(docID)
 
-      });
-    });
+      promise.then (newDoctor => {
+        let doctor = JSON.parse(newDoctor);
+        $(".doctorSearch").append(`<h1>${doctor.data.profile.first_name}${doctor.data.profile.last_name}</h1>`)
+        $(".doctorSearch").append(`<h3>Specialties</h3>`)
+        for (let i in doctor.data.specialties) {
+          $(".doctorSearch").append(`<p>${doctor.data.specialties[i].name}</p>`)
+        }
+        $(".doctorSearch").append(`<h3>Practices</h3>`)
+        for (let i in doctor.data.practices) {
+          $(".doctorSearch").append(`<hr /><p>accepts new patients: (${doctor.data.practices[i].accepts_new_patients})</p>`)
+          $(".doctorSearch").append(`<p>phone: ${doctor.data.practices[i].phones[0].number}</p>`)
+          $(".doctorSearch").append(`<p>address:
+            ${doctor.data.practices[i].visit_address.street}
+            ${doctor.data.practices[i].visit_address.city}
+            ${doctor.data.practices[i].visit_address.state}
+            ${doctor.data.practices[i].visit_address.zip}
+            </p>`)
+        }
+
+
+        console.log(doctor)
+      })
+    }
+
+    // $(".clickable").each(function() {
+    //   $(this).click(function() {
+    //     console.log("clicked")
+    //     findDoctor(this.id)
+    //
+    //   });
+    // });
 
   })
 
